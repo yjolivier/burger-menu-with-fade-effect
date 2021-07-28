9e78febe518c1015d83bbda4ad6c8a248204b6da
yjolivier <yaojolivier@gmail.com>
2021-07-28 17:50:46 +0000

    Try to make animation with timeline

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const menuBtn = document.getElementById('menu_burger_id'),
-menuContent = document.getElementById('menu_nav_id');
+menuContent = document.getElementById('menu_nav_id'),
+menuContentCardRight = menuContent.querySelectorAll('ul.menu_right'),
+menuContentCardRightLis = menuContent.querySelectorAll('ul.menu_right li'),
+menuContentCardRightTitle = menuContent.querySelector('ul.menu_right li.menu_right-title'),
+menuContentCardLeft = menuContent.querySelector('ul.menu_left');
 let isActive = false;
 
 menuBtn.addEventListener('click', () => {
     if(!isActive){
         menuBtn.classList.add('active');
-        menu_nav_id.classList.add('active');
+        menuContent.classList.add('active');
         isActive = true;
+        setTimeout(() => {
+            menuContentCardRightTitle.style.marginTop = "0px";
+            for(i = 0; i < menuContentCardRightLis.length; i++){
+                menuContentCardRightLis[i].style.opacity = "1";
+            }
+            setTimeout(() =>{
+                menuContentCardRight.style.marginLeft = "0px";
+                menuContentCardLeft.style.width = "30%";
+            },500)
+        }, 100);
     }
     else{
         menuBtn.classList.remove('active');
