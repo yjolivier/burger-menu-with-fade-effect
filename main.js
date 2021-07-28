ed7bae97c12dec51cca62e02eb3b7e7b51499f88
yjolivier <yaojolivier@gmail.com>
2021-07-28 08:57:51 +0000

    Fix(): Menu animation

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-const menuBtn = document.getElementById('menu_burger_id');
+const menuBtn = document.getElementById('menu_burger_id'),
+menuContent = document.getElementById('menu_nav_id');
 let isActive = false;
 
 menuBtn.addEventListener('click', () => {
     if(!isActive){
         menuBtn.classList.add('active');
+        menu_nav_id.classList.add('active');
         isActive = true;
     }
     else{
         menuBtn.classList.remove('active');
+        menu_nav_id.classList.remove('active');
         isActive = false;
     }
 })
